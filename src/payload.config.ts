c3000cf29582f3a84f282111bd3c4e043eac86c3
tdavande <tdavande@outlook.com>
2026-01-13 17:57:43 +0000

    removed references to sql lite

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -1,24 +1,20 @@
-// import { postgresAdapter } from '@payloadcms/db-postgres'
-import { sqliteAdapter } from '@payloadcms/db-sqlite' // Add this
+import { postgresAdapter } from '@payloadcms/db-postgres'
 import { lexicalEditor } from '@payloadcms/richtext-lexical'
 import path from 'path'
 import { buildConfig } from 'payload'
 import { fileURLToPath } from 'url'
 import sharp from 'sharp'
-import { Pool, neonConfig } from '@neondatabase/serverless' // Added for WebSocket support
-import ws from 'ws' // Added for Node.js WebSocket support
 import dotenv from 'dotenv'
+
 dotenv.config({
   path: path.resolve(process.cwd(), '.env'),
 })
-console.log('--- DEBUG: IS SECRET LOADED? ---', process.env.PAYLOAD_SECRET ? 'YES' : 'NO')
+
 import { Users } from './collections/Users'
 import { Media } from './collections/Media'
 
 const filename = fileURLToPath(import.meta.url)
 const dirname = path.dirname(filename)
-
-neonConfig.webSocketConstructor = ws 
 
 export default buildConfig({
   admin: {
@@ -30,19 +26,10 @@ export default buildConfig({
   collections: [Users, Media],
   editor: lexicalEditor(),
   secret: process.env.PAYLOAD_SECRET || '',
-  typescript: {
-    outputFile: path.resolve(dirname, 'payload-types.ts'),
-  },
-/*   db: postgresAdapter({
-    pool: new Pool({
+  db: postgresAdapter({
+    pool: {
       connectionString: process.env.DATABASE_URL || '',
-    }),
-  }), */
-  db: sqliteAdapter({
-    client: {
-      url: 'file:./stinky.db',
     },
   }),
   sharp,
-  plugins: [],
 })
